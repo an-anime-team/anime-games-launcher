13b13c5b7ef14a45a3001c4233b2897ba82d04f8
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2024-01-07 17:13:12 +0200

    feat: allowed to launch the game

--- src/ui/windows/main/launch_game.rs
+++ src/ui/windows/main/launch_game.rs
@@ -7,44 +7,38 @@ use crate::components::wine::Wine;
 use crate::ui::components::game_card::CardInfo;
 
 pub fn launch_game(info: &CardInfo) -> anyhow::Result<()> {
-    // // Get game driver
-    // let Some(game) = games::get(info.get_name())? else {
-    //     anyhow::bail!("Unable to find {} integration script", info.get_title());
-    // };
+    // Get game driver
+    let game = unsafe {
+        games::get_unsafe(info.get_name())
+    };
 
-    // // Get game settings
-    // let settings = config::get().games.get_game_settings(info.get_name())?;
+    // Get game settings
+    let settings = config::get().games.get_game_settings(info.get_name())?;
 
-    // // Get installation folder driver
-    // let Some(paths) = settings.paths.get(info.get_edition()) else {
-    //     anyhow::bail!("Unable to find {} installation path", info.get_title());
-    // };
+    // Get installation folder driver
+    let Some(paths) = settings.paths.get(info.get_edition()) else {
+        anyhow::bail!("Unable to find {} installation path", info.get_title());
+    };
 
-    // let driver = paths.game.to_dyn_trait();
+    // Request game launch options
+    let options = game.get_launch_options(paths.game.to_string_lossy(), info.get_edition())?;
 
-    // // Deploy the game
-    // let path = driver.deploy()?;
+    // Prepare launch command
+    let command = [
+        format!("{:?}", Wine::from_config()?.get_executable()),
+        format!("{:?}", paths.game.join(options.executable))
+    ];
 
-    // // Request game launch options
-    // let options = game.get_launch_options(path.to_string_lossy(), info.get_edition())?;
+    // TODO: support addons
 
-    // // Prepare launch command
-    // let command = [
-    //     format!("{:?}", Wine::from_config()?.get_executable()),
-    //     format!("{:?}", path.join(options.executable))
-    // ];
-
-    // // Run the game
-    // Command::new("bash")
-    //     .arg("-c")
-    //     .arg(command.join(" "))
-    //     .envs(options.environment)
-    //     .current_dir(path)
-    //     .spawn()?
-    //     .wait()?;
-
-    // // Dismantle installation fodler
-    // driver.dismantle()?;
+    // Run the game
+    Command::new("bash")
+        .arg("-c")
+        .arg(command.join(" "))
+        .envs(options.environment)
+        .current_dir(&paths.game)
+        .spawn()?
+        .wait()?;
 
     Ok(())
 }
