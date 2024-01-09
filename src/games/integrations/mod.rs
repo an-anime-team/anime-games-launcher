4111b7e3640e00c03c3dfd9e7a94a615a4216479
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2024-01-10 01:18:26 +0200

    refactor: reduced overhead in game lua driver

--- src/games/integrations/mod.rs
+++ src/games/integrations/mod.rs
@@ -63,25 +63,21 @@ impl Game {
     pub fn get_card_picture(&self, edition: impl AsRef<str>) -> anyhow::Result<String> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals()
-                .get::<_, LuaFunction>("v1_visual_get_card_picture")?
-                .call::<_, String>(edition.as_ref())?)
+                .call_function("v1_visual_get_card_picture", edition.as_ref())?)
         }
     }
 
     pub fn get_background_picture(&self, edition: impl AsRef<str>) -> anyhow::Result<String> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals()
-                .get::<_, LuaFunction>("v1_visual_get_background_picture")?
-                .call::<_, String>(edition.as_ref())?)
+                .call_function("v1_visual_get_background_picture", edition.as_ref())?)
         }
     }
 
     pub fn get_details_background_style(&self, edition: impl AsRef<str>) -> anyhow::Result<Option<String>> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals().contains_key("v1_visual_get_details_background_css")?
-                .then(|| self.lua.globals().get::<_, LuaFunction>("v1_visual_get_details_background_css"))
-                .transpose()?
-                .map(|function| function.call::<_, String>(edition.as_ref()))
+                .then(|| self.lua.globals().call_function("v1_visual_get_details_background_css", edition.as_ref()))
                 .transpose()?)
         }
     }
@@ -90,8 +86,7 @@ impl Game {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => {
                 let editions = self.lua.globals()
-                    .get::<_, LuaFunction>("v1_game_get_editions_list")?
-                    .call::<_, LuaTable>(())?
+                    .call_function::<_, LuaTable>("v1_game_get_editions_list", ())?
                     .sequence_values::<LuaTable>()
                     .flatten()
                     .flat_map(|edition| GameEdition::from_table(edition, self.manifest.script_standard))
@@ -105,16 +100,14 @@ impl Game {
     pub fn is_game_installed(&self, path: impl AsRef<str>) -> anyhow::Result<bool> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals()
-                .get::<_, LuaFunction>("v1_game_is_installed")?
-                .call::<_, bool>(path.as_ref())?)
+                .call_function("v1_game_is_installed", path.as_ref())?)
         }
     }
 
     pub fn get_game_version(&self, path: impl AsRef<str>, edition: impl AsRef<str>) -> anyhow::Result<Option<String>> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals()
-                .get::<_, LuaFunction>("v1_game_get_version")?
-                .call::<_, Option<String>>((path.as_ref(), edition.as_ref()))?)
+                .call_function("v1_game_get_version", (path.as_ref(), edition.as_ref()))?)
         }
     }
 
@@ -122,8 +115,7 @@ impl Game {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => {
                 let download = self.lua.globals()
-                    .get::<_, LuaFunction>("v1_game_get_download")?
-                    .call::<_, LuaTable>(edition.as_ref())?;
+                    .call_function("v1_game_get_download", edition.as_ref())?;
 
                 Download::from_table(download, self.manifest.script_standard)
             }
@@ -134,8 +126,7 @@ impl Game {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => {
                 let diff = self.lua.globals()
-                    .get::<_, LuaFunction>("v1_game_get_diff")?
-                    .call::<_, Option<LuaTable>>((path.as_ref(), edition.as_ref()))?;
+                    .call_function("v1_game_get_diff", (path.as_ref(), edition.as_ref()))?;
 
                 match diff {
                     Some(diff) => Ok(Some(Diff::from_table(diff, self.manifest.script_standard)?)),
@@ -149,8 +140,7 @@ impl Game {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => {
                 let status = self.lua.globals()
-                    .get::<_, LuaFunction>("v1_game_get_status")?
-                    .call::<_, Option<LuaTable>>((path.as_ref(), edition.as_ref()))?;
+                    .call_function("v1_game_get_status", (path.as_ref(), edition.as_ref()))?;
 
                 match status {
                     Some(status) => Ok(Some(GameStatus::from_table(status, self.manifest.script_standard)?)),
@@ -164,8 +154,7 @@ impl Game {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => {
                 let options = self.lua.globals()
-                    .get::<_, LuaFunction>("v1_game_get_launch_options")?
-                    .call::<_, LuaTable>((game_path.as_ref(), addons_path.as_ref(), edition.as_ref()))?;
+                    .call_function("v1_game_get_launch_options", (game_path.as_ref(), addons_path.as_ref(), edition.as_ref()))?;
 
                 GameLaunchOptions::from_table(options, self.manifest.script_standard)
             }
@@ -176,8 +165,7 @@ impl Game {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => {
                 let dlcs = self.lua.globals()
-                    .get::<_, LuaFunction>("v1_addons_get_list")?
-                    .call::<_, LuaTable>(edition.as_ref())?
+                    .call_function::<_, LuaTable>("v1_addons_get_list", edition.as_ref())?
                     .sequence_values::<LuaTable>()
                     .flatten()
                     .flat_map(|group| AddonsGroup::from_table(group, self.manifest.script_standard))
@@ -191,8 +179,7 @@ impl Game {
     pub fn is_addon_installed(&self, group_name: impl AsRef<str>, addon_name: impl AsRef<str>, addon_path: impl AsRef<str>, edition: impl AsRef<str>) -> anyhow::Result<bool> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals()
-                .get::<_, LuaFunction>("v1_addons_is_installed")?
-                .call::<_, bool>((
+                .call_function("v1_addons_is_installed", (
                     group_name.as_ref(),
                     addon_name.as_ref(),
                     addon_path.as_ref(),
@@ -204,8 +191,7 @@ impl Game {
     pub fn get_addon_version(&self, group_name: impl AsRef<str>, addon_name: impl AsRef<str>, addon_path: impl AsRef<str>, edition: impl AsRef<str>) -> anyhow::Result<Option<String>> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals()
-                .get::<_, LuaFunction>("v1_addons_get_version")?
-                .call::<_, Option<String>>((
+                .call_function("v1_addons_get_version", (
                     group_name.as_ref(),
                     addon_name.as_ref(),
                     addon_path.as_ref(),
@@ -218,8 +204,7 @@ impl Game {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => {
                 let download = self.lua.globals()
-                    .get::<_, LuaFunction>("v1_addons_get_download")?
-                    .call::<_, LuaTable>((
+                    .call_function("v1_addons_get_download", (
                         group_name.as_ref(),
                         addon_name.as_ref(),
                         edition.as_ref()
@@ -234,8 +219,7 @@ impl Game {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => {
                 let diff = self.lua.globals()
-                    .get::<_, LuaFunction>("v1_addons_get_diff")?
-                    .call::<_, Option<LuaTable>>((
+                    .call_function("v1_addons_get_diff", (
                         group_name.as_ref(),
                         addon_name.as_ref(),
                         addon_path.as_ref(),
@@ -253,8 +237,7 @@ impl Game {
     pub fn get_addon_paths(&self, group_name: impl AsRef<str>, addon_name: impl AsRef<str>, addon_path: impl AsRef<str>, edition: impl AsRef<str>) -> anyhow::Result<Vec<String>> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals()
-                .get::<_, LuaFunction>("v1_addons_get_paths")?
-                .call::<_, Vec<String>>((
+                .call_function("v1_addons_get_paths", (
                     group_name.as_ref(),
                     addon_name.as_ref(),
                     addon_path.as_ref(),
@@ -272,8 +255,7 @@ impl Game {
     pub fn run_game_diff_transition(&self, transition_path: impl AsRef<str>, edition: impl AsRef<str>) -> anyhow::Result<()> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals()
-                .get::<_, LuaFunction>("v1_game_diff_transition")?
-                .call::<_, ()>((transition_path.as_ref(), edition.as_ref()))?)
+                .call_function("v1_game_diff_transition", (transition_path.as_ref(), edition.as_ref()))?)
         }
     }
 
@@ -286,8 +268,7 @@ impl Game {
     pub fn run_game_diff_post_transition(&self, path: impl AsRef<str>, edition: impl AsRef<str>) -> anyhow::Result<()> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals()
-                .get::<_, LuaFunction>("v1_game_diff_post_transition")?
-                .call::<_, ()>((path.as_ref(), edition.as_ref()))?)
+                .call_function("v1_game_diff_post_transition", (path.as_ref(), edition.as_ref()))?)
         }
     }
 
@@ -300,8 +281,12 @@ impl Game {
     pub fn run_addons_diff_transition(&self, group_name: impl AsRef<str>, addon_name: impl AsRef<str>, transition_path: impl AsRef<str>, edition: impl AsRef<str>) -> anyhow::Result<()> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals()
-                .get::<_, LuaFunction>("v1_addons_diff_transition")?
-                .call::<_, ()>((group_name.as_ref(), addon_name.as_ref(), transition_path.as_ref(), edition.as_ref()))?)
+                .call_function("v1_addons_diff_transition", (
+                    group_name.as_ref(),
+                    addon_name.as_ref(),
+                    transition_path.as_ref(),
+                    edition.as_ref()
+                ))?)
         }
     }
 
@@ -314,8 +299,12 @@ impl Game {
     pub fn run_addons_diff_post_transition(&self, group_name: impl AsRef<str>, addon_name: impl AsRef<str>, addon_path: impl AsRef<str>, edition: impl AsRef<str>) -> anyhow::Result<()> {
         match self.manifest.script_standard {
             IntegrationStandard::V1 => Ok(self.lua.globals()
-                .get::<_, LuaFunction>("v1_addons_diff_post_transition")?
-                .call::<_, ()>((group_name.as_ref(), addon_name.as_ref(), addon_path.as_ref(), edition.as_ref()))?)
+                .call_function("v1_addons_diff_post_transition", (
+                    group_name.as_ref(),
+                    addon_name.as_ref(),
+                    addon_path.as_ref(),
+                    edition.as_ref()
+                ))?)
         }
     }
 }
