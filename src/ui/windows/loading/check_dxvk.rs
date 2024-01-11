f232e71a419f446d26c47352e9a3687b33f5bf77
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2024-01-12 00:57:50 +0200

    fix: fixed `check_dxvk::get_apply` loading task

--- src/ui/windows/loading/check_dxvk.rs
+++ src/ui/windows/loading/check_dxvk.rs
@@ -16,15 +16,22 @@ pub fn get_download() -> anyhow::Result<Option<Dxvk>> {
 
 #[inline]
 pub fn get_apply() -> anyhow::Result<Option<Dxvk>> {
-    let dxvk = Dxvk::from_config()?;
+    let prefix = config::get().components.wine.prefix.path;
 
-    let installed_dxvk = WincompatlibDxvk::get_version(config::get().components.wine.prefix.path)?;
+    if !prefix.exists() {
+        // We don't need to apply DXVK because it's done during prefix creation
+        return Ok(None);
+    }
+
+    let selected_dxvk = Dxvk::from_config()?;
+
+    let installed_dxvk = WincompatlibDxvk::get_version(prefix)?;
 
     if let Some(version) = installed_dxvk {
-        if dxvk.name.contains(&version) || dxvk.version.contains(&version) {
+        if selected_dxvk.name.contains(&version) || selected_dxvk.version.contains(&version) {
             return Ok(None);
         }
     }
 
-    Ok(Some(dxvk))
+    Ok(Some(selected_dxvk))
 }
