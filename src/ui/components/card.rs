3471429e72a23e52ea16923fb7f39783157e1b69
Dylan Donnell <dylan.donnell@student.griffith.ie>
2024-09-16 14:46:48 +0100

    feat(ui): CardsComponentFactory

--- src/ui/components/card.rs
+++ src/ui/components/card.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use relm4::prelude::*;
+use relm4::{factory::AsyncFactoryComponent, prelude::*, AsyncFactorySender};
 
 // 10:14
 pub const DEFAULT_SIZE: (i32, i32) = (240, 336);
@@ -171,6 +171,135 @@ impl SimpleAsyncComponent for CardComponent {
             CardComponentInput::SetBlurred(blurred) => self.blurred = blurred,
 
             CardComponentInput::EmitClick => sender.output(CardComponentOutput::Clicked).unwrap(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct CardComponentFactory {
+    pub image: Option<String>,
+    pub title: Option<String>,
+
+    pub width: i32,
+    pub height: i32,
+
+    pub clickable: bool,
+    pub blurred: bool,
+
+    index: DynamicIndex,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum CardComponentFactoryOutput {
+    Clicked(DynamicIndex),
+}
+
+#[relm4::factory(pub, async)]
+impl AsyncFactoryComponent for CardComponentFactory {
+    type Init = CardComponent;
+    type Input = CardComponentInput;
+    type Output = CardComponentFactoryOutput;
+    type ParentWidget = gtk::Box;
+    type CommandOutput = ();
+
+    view! {
+        #[root]
+        adw::Clamp {
+            #[watch]
+            set_maximum_size: self.width,
+
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+
+                gtk::Overlay {
+                    #[watch]
+                    set_tooltip?: &self.title,
+
+                    gtk::Picture {
+                        set_valign: gtk::Align::Start,
+                        set_halign: gtk::Align::Start,
+
+                        set_content_fit: gtk::ContentFit::Cover,
+
+                        add_css_class: "card",
+
+                        #[watch]
+                        set_size_request: (self.width, self.height),
+
+                        #[watch]
+                        set_opacity: if self.blurred { 0.4 } else { 1.0 },
+
+                        #[watch]
+                        set_resource?: self.image.as_ref()
+                            .and_then(|image| image
+                                .starts_with(crate::APP_RESOURCE_PREFIX)
+                                .then_some(Some(image.as_str()))),
+
+                        #[watch]
+                        set_filename?: self.image.as_ref()
+                            .and_then(|image| (!image
+                                .starts_with(crate::APP_RESOURCE_PREFIX))
+                                .then_some(Some(image.as_str())))
+                    },
+
+                    add_overlay = &gtk::Button {
+                        add_css_class: "flat",
+
+                        #[watch]
+                        set_visible: self.clickable,
+
+                        connect_clicked => CardComponentInput::EmitClick
+                    }
+                },
+
+                gtk::Box {
+                    set_orientation: gtk::Orientation::Horizontal,
+                    set_halign: gtk::Align::Center,
+
+                    set_margin_all: 12,
+
+                    #[watch]
+                    set_visible: self.title.is_some(),
+
+                    gtk::Label {
+                        #[watch]
+                        set_label?: &self.title
+                    }
+                }
+            }
+        }
+    }
+
+    async fn init_model(
+        init: Self::Init,
+        index: &DynamicIndex,
+        sender: AsyncFactorySender<Self>,
+    ) -> Self {
+        Self {
+            image: init.image,
+            title: init.title,
+            width: init.width,
+            height: init.height,
+            clickable: init.clickable,
+            blurred: init.blurred,
+            index: index.clone(),
+        }
+    }
+
+    async fn update(&mut self, msg: Self::Input, sender: AsyncFactorySender<Self>) {
+        match msg {
+            CardComponentInput::SetTitle(title) => self.title = title,
+            CardComponentInput::SetImage(image) => self.image = image,
+
+            CardComponentInput::SetWidth(width) => self.width = width,
+            CardComponentInput::SetHeight(height) => self.height = height,
+
+            CardComponentInput::SetClickable(clickable) => self.clickable = clickable,
+            CardComponentInput::SetBlurred(blurred) => self.blurred = blurred,
+
+            CardComponentInput::EmitClick => sender
+                .output(CardComponentFactoryOutput::Clicked(self.index.clone()))
+                .unwrap(),
         }
     }
 }
