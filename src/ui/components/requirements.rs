fde7ccec3da0e206e46ff4e41ea0e70d647be5cb
Dylan Donnell <dylan.donnell@student.griffith.ie>
2024-09-19 14:51:15 +0100

    fix(ui): display optimal HardwareRequirements only if present

--- src/ui/components/requirements.rs
+++ src/ui/components/requirements.rs
@@ -3,7 +3,7 @@ use gtk::prelude::*;
 
 use relm4::prelude::*;
 
-use hardware_requirements::GameHardwareRequirements;
+use hardware_requirements::{requirements::HardwareRequirements, GameHardwareRequirements};
 use unic_langid::LanguageIdentifier;
 
 use crate::{
@@ -13,10 +13,113 @@ use crate::{
 
 const DIM_CLASS: &[&str] = &["dim-label"];
 
+#[relm4::component(async, pub)]
+impl SimpleAsyncComponent for HardwareRequirements {
+    type Init = (HardwareRequirements, LanguageIdentifier);
+    type Input = ();
+    type Output = ();
+
+    view! {
+        #[root]
+        gtk::Box {
+            set_hexpand: true,
+            set_vexpand: true,
+
+            adw::PreferencesGroup {
+                set_hexpand: true,
+                adw::ExpanderRow {
+                    set_title: "Processor",
+                    add_suffix = &gtk::Label {
+                    set_label: &model.cpu.as_ref().unwrap().model.translate(&lang),
+                        set_css_classes: DIM_CLASS,
+                    },
+                    add_row = &adw::ActionRow {
+                        set_title: "Core Count",
+                        add_suffix = &gtk::Label {
+                            set_label: &model.cpu.as_ref().unwrap().cores.unwrap().to_string(),
+                            set_css_classes: DIM_CLASS,
+                        }
+                    },
+                    add_row = &adw::ActionRow {
+                        set_title: "Frequency",
+                        add_suffix = &gtk::Label {
+                            set_label: &pretty_frequency(model.cpu.as_ref().unwrap().frequency.unwrap()),
+                            set_css_classes: DIM_CLASS,
+                        }
+                    }
+                },
+
+                adw::ExpanderRow {
+                    set_title: "Graphics",
+                    add_suffix = &gtk::Label {
+                        set_label: &model.gpu.as_ref().unwrap().model.translate(&lang),
+                        set_css_classes: DIM_CLASS,
+                    },
+                    add_row = &adw::ActionRow {
+                        set_title: "Video Memory",
+                        add_suffix = &gtk::Label {
+                            set_label: &pretty_bytes(model.gpu.as_ref().unwrap().vram.unwrap()),
+                            set_css_classes: DIM_CLASS,
+                        }
+                    }
+                },
+
+                adw::ExpanderRow {
+                    set_title: "Memory",
+                    add_suffix = &gtk::Label {
+                        set_label: &model.cpu.as_ref().unwrap().model.translate(&lang),
+                        set_css_classes: DIM_CLASS,
+                    },
+                    add_row = &adw::ActionRow {
+                        set_title: "Capacity",
+                        add_suffix = &gtk::Label {
+                            set_label: &pretty_bytes(model.ram.as_ref().unwrap().size),
+                            set_css_classes: DIM_CLASS,
+                        }
+                    },
+                    add_row = &adw::ActionRow {
+                        set_title: "Frequency",
+                        add_suffix = &gtk::Label {
+                            set_label: &pretty_frequency(model.ram.as_ref().unwrap().frequency.unwrap()),
+                            set_css_classes: DIM_CLASS,
+                        }
+                    }
+                },
+
+                adw::ExpanderRow {
+                    set_title: "Disk",
+                    add_suffix = &gtk::Label {
+                        set_label: &pretty_bytes(model.disk.as_ref().unwrap().size),
+                        set_css_classes: DIM_CLASS,
+                    },
+                    add_row = &adw::ActionRow {
+                        set_title: "Type",
+                        add_suffix = &gtk::Label {
+                            set_label: &format!("{}", model.disk.as_ref().unwrap().disk_type.clone().unwrap()).to_uppercase(),
+                            set_css_classes: DIM_CLASS,
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    async fn init(
+        init: Self::Init,
+        root: Self::Root,
+        sender: AsyncComponentSender<Self>,
+    ) -> AsyncComponentParts<Self> {
+        let model = init.0;
+        let lang = init.1;
+        let widgets = view_output!();
+        AsyncComponentParts { model, widgets }
+    }
+}
+
 #[derive(Debug)]
 pub struct RequirementsComponent {
-    pub req: GameHardwareRequirements,
-    pub lang: LanguageIdentifier,
+    pub minimal: AsyncController<HardwareRequirements>,
+    pub optimal: Option<AsyncController<HardwareRequirements>>,
 }
 
 #[relm4::component(async, pub)]
@@ -39,176 +142,12 @@ impl SimpleAsyncComponent for RequirementsComponent {
             #[local_ref]
             view_stack -> adw::ViewStack {
                 add = &gtk::Box {
-                    set_hexpand: true,
-                    set_vexpand: true,
-
-                    adw::PreferencesGroup {
-                        set_hexpand: true,
-                        adw::ExpanderRow {
-                            set_title: "Processor",
-                            add_suffix = &gtk::Label {
-                            set_label: &model.req.minimal.cpu.as_ref().unwrap().model.translate(&model.lang),
-                                set_css_classes: DIM_CLASS,
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Core Count",
-                                add_suffix = &gtk::Label {
-                                    set_label: &model.req.minimal.cpu.as_ref().unwrap().cores.unwrap().to_string(),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Frequency",
-                                add_suffix = &gtk::Label {
-                                    set_label: &pretty_frequency(model.req.minimal.cpu.as_ref().unwrap().frequency.unwrap()),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            }
-                        },
-
-                        adw::ExpanderRow {
-                            set_title: "Graphics",
-                            add_suffix = &gtk::Label {
-                                set_label: &model.req.minimal.gpu.as_ref().unwrap().model.translate(&model.lang),
-                                set_css_classes: DIM_CLASS,
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Video Memory",
-                                add_suffix = &gtk::Label {
-                                    set_label: &pretty_bytes(model.req.minimal.gpu.as_ref().unwrap().vram.unwrap()),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            }
-                        },
-
-                        adw::ExpanderRow {
-                            set_title: "Memory",
-                            add_suffix = &gtk::Label {
-                                set_label: &model.req.minimal.cpu.as_ref().unwrap().model.translate(&model.lang),
-                                set_css_classes: DIM_CLASS,
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Capacity",
-                                add_suffix = &gtk::Label {
-                                    set_label: &pretty_bytes(model.req.minimal.ram.as_ref().unwrap().size),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Frequency",
-                                add_suffix = &gtk::Label {
-                                    set_label: &pretty_frequency(model.req.minimal.ram.as_ref().unwrap().frequency.unwrap()),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            }
-                        },
-
-                        adw::ExpanderRow {
-                            set_title: "Disk",
-                            add_suffix = &gtk::Label {
-                                set_label: &pretty_bytes(model.req.minimal.disk.as_ref().unwrap().size),
-                                set_css_classes: DIM_CLASS,
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Type",
-                                add_suffix = &gtk::Label {
-                                    set_label: &format!("{}", model.req.minimal.disk.as_ref().unwrap().disk_type.clone().unwrap()).to_uppercase(),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            }
-                        }
-                    }
+                    model.minimal.widget(),
                 } -> {
                     set_name: Some("Minimum"),
                     set_title: Some("Minimum"),
                     set_icon_name: Some("speedometer4-symbolic"),
                 },
-
-                add = &gtk::Box {
-                    set_hexpand: true,
-                    set_vexpand: true,
-
-                    adw::PreferencesGroup {
-                        set_hexpand: true,
-                        adw::ExpanderRow {
-                            set_title: "Processor",
-                            add_suffix = &gtk::Label {
-                            set_label: &opt.clone().unwrap().cpu.as_ref().unwrap().model.translate(&model.lang),
-                                set_css_classes: DIM_CLASS,
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Core Count",
-                                add_suffix = &gtk::Label {
-                                    set_label: &opt.clone().unwrap().cpu.as_ref().unwrap().cores.unwrap().to_string(),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Frequency",
-                                add_suffix = &gtk::Label {
-                                    set_label: &pretty_frequency(opt.clone().unwrap().cpu.as_ref().unwrap().frequency.unwrap()),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            }
-                        },
-
-                        adw::ExpanderRow {
-                            set_title: "Graphics",
-                            add_suffix = &gtk::Label {
-                                set_label: &opt.clone().unwrap().gpu.as_ref().unwrap().model.translate(&model.lang),
-                                set_css_classes: DIM_CLASS,
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Video Memory",
-                                add_suffix = &gtk::Label {
-                                    set_label: &pretty_bytes(opt.clone().unwrap().gpu.as_ref().unwrap().vram.unwrap()),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            }
-                        },
-
-                        adw::ExpanderRow {
-                            set_title: "Memory",
-                            add_suffix = &gtk::Label {
-                                set_label: &opt.clone().unwrap().cpu.as_ref().unwrap().model.translate(&model.lang),
-                                set_css_classes: DIM_CLASS,
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Capacity",
-                                add_suffix = &gtk::Label {
-                                    set_label: &pretty_bytes(opt.clone().unwrap().ram.as_ref().unwrap().size),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Frequency",
-                                add_suffix = &gtk::Label {
-                                    set_label: &pretty_frequency(opt.clone().unwrap().ram.as_ref().unwrap().frequency.unwrap()),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            }
-                        },
-
-                        adw::ExpanderRow {
-                            set_title: "Disk",
-                            add_suffix = &gtk::Label {
-                                set_label: &pretty_bytes(opt.clone().unwrap().disk.as_ref().unwrap().size),
-                                set_css_classes: DIM_CLASS,
-                            },
-                            add_row = &adw::ActionRow {
-                                set_title: "Type",
-                                add_suffix = &gtk::Label {
-                                    set_label: &format!("{}", opt.clone().unwrap().disk.as_ref().unwrap().disk_type.clone().unwrap()).to_uppercase(),
-                                    set_css_classes: DIM_CLASS,
-                                }
-                            }
-                        }
-                    }
-                } -> {
-                    set_name: Some("Recommended"),
-                    set_title: Some("Recommended"),
-                    set_icon_name: Some("speedometer2-symbolic"),
-                }
             }
         }
     }
@@ -219,12 +158,31 @@ impl SimpleAsyncComponent for RequirementsComponent {
         sender: AsyncComponentSender<Self>,
     ) -> AsyncComponentParts<Self> {
         let view_stack = adw::ViewStack::new();
-        let model = Self {
-            req: init,
-            lang: LanguageIdentifier::default(),
+        let lang = LanguageIdentifier::default();
+
+        let mut model = Self {
+            minimal: HardwareRequirements::builder()
+                .launch((init.minimal, lang.clone()))
+                .detach(),
+            optimal: None,
         };
-        let opt = &model.req.optimal;
+
+        // Insert recommended only if present
+        if let Some(optimal) = init.optimal {
+            model.optimal = Some(
+                HardwareRequirements::builder()
+                    .launch((optimal, lang))
+                    .detach(),
+            );
+        }
+        if let Some(req) = &model.optimal {
+            view_stack
+                .add_titled(req.widget(), None, "Recommended")
+                .set_icon_name(Some("speedometer2-symbolic"));
+        }
+
         let widgets = view_output!();
+
         AsyncComponentParts { model, widgets }
     }
 }
