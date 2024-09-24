dfccfce2fdf211ad04e8265f6aaf33e7d9eeace7
Nikita Podvirnyi <krypt0nn@vk.com>
2024-09-24 13:32:52 +0200

    tests: fixed modules loading tests

--- tests/packages/1/module.lua
+++ tests/packages/1/module.lua
@@ -3,11 +3,11 @@ local package = load("another-package-module")
 return {
     greeting = package.value.hello("World"),
 
-    test_load_valid_input = load("another-package"),
-    test_load_valid_output = load("self-reference"),
+    test_load_valid_input = pcall(load("another-package")),
+    test_load_valid_output = pcall(load("module")),
 
-    test_load_invalid_input = load("file-reference"),
-    test_load_invalid_output = load("dxvk"),
+    test_load_invalid_input = pcall(load("file-reference")),
+    test_load_invalid_output = pcall(load("dxvk")),
 
-    test_load_unexisting_input = load("amogus")
+    test_load_unexisting_input = pcall(load("amogus"))
 }
