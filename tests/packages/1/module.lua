43d5f3d9b9683638fd904ec69b3baebd86d4f55a
Nikita Podvirnyi <krypt0nn@vk.com>
2024-09-24 13:25:33 +0200

    tests: added test cases to the module loading

--- tests/packages/1/module.lua
+++ tests/packages/1/module.lua
@@ -1,5 +1,13 @@
 local package = load("another-package-module")
 
 return {
-    greeting = package.value.hello("World")
+    greeting = package.value.hello("World"),
+
+    test_load_valid_input = load("another-package"),
+    test_load_valid_output = load("self-reference"),
+
+    test_load_invalid_input = load("file-reference"),
+    test_load_invalid_output = load("dxvk"),
+
+    test_load_unexisting_input = load("amogus")
 }
