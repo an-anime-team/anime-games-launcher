ce3a61a5d47d29206b552962c99f83054b9760de
Nikita Podvirnyi <krypt0nn@vk.com>
2024-10-18 21:28:46 +0200

    build: updated `flake.nix`

--- flake.nix
+++ flake.nix
@@ -3,11 +3,24 @@
 
     inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
+    nixConfig = {
+        extra-substituters = [
+            "https://cache.nixos.org"
+            "https://nix-community.cachix.org"
+            "https://krypt0nn.cachix.org"
+        ];
+
+        extra-trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "krypt0nn.cachix.org-1:ciP8xHjGQDDEjSW1LL9PO/fn8JRzm8zb57eUcFAblR8="
+        ];
+    };
+
     outputs = { self, nixpkgs }:
         let
             system = "x86_64-linux";
 
-            # pkgs = (import nixpkgs { inherit system; }).pkgsStatic;
             pkgs = import nixpkgs { inherit system; };
 
             config = pkgs.lib.importTOML ./Cargo.toml;
@@ -40,17 +53,19 @@
                 ];
 
                 buildInputs = with pkgs; [
+                    libadwaita
                     gtk4
                     gdk-pixbuf
                     gobject-introspection
 
-                    libadwaita
                     openssl
                     luau
                 ];
 
                 # CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.llvmPackages.clangUseLLVM}/bin/clang";
-                # CARGO_ENCODED_RUSTFLAGS = "-Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold";
+                # CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${pkgs.llvmPackages.clangUseLLVM}/bin/clang";
+
+                # CARGO_ENCODED_RUSTFLAGS = "-Clink-arg=--ld-path=${pkgs.mold}/bin/mold";
             };
         };
 }
