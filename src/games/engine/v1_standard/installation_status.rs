b43c375585b333963088bd9b81efcdaa85e4091b
Nikita Podvirnyi <krypt0nn@vk.com>
2025-04-18 14:07:56 +0200

    feat(games): remove `update-unavailable` installation status
    This is a bad design and if something is not easily
    available to the user then integration module must
    implement some logic to fix this.

--- src/games/engine/v1_standard/installation_status.rs
+++ src/games/engine/v1_standard/installation_status.rs
@@ -2,22 +2,16 @@ use mlua::prelude::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum InstallationStatus {
-    /// Latest component version is installed.
+    /// Latest game version is installed.
     Installed,
 
-    /// Component is installed but there's an
-    /// optional update available.
+    /// Game is installed but there's an optional update available.
     UpdateAvailable,
 
-    /// Component is installed but there's an update
-    /// available that must be installed.
+    /// Game is installed but there's an update available that must be installed.
     UpdateRequired,
 
-    /// Component is installed but there's an update
-    /// which cannot be installed automatically.
-    UpdateUnavailable,
-
-    /// Component is not installed.
+    /// Game is not installed.
     NotInstalled
 }
 
@@ -27,7 +21,6 @@ impl std::fmt::Display for InstallationStatus {
             Self::Installed         => f.write_str("installed"),
             Self::UpdateAvailable   => f.write_str("update-available"),
             Self::UpdateRequired    => f.write_str("update-required"),
-            Self::UpdateUnavailable => f.write_str("update-unavailable"),
             Self::NotInstalled      => f.write_str("not-installed")
         }
     }
@@ -41,7 +34,6 @@ impl std::str::FromStr for InstallationStatus {
             "installed"          => Ok(Self::Installed),
             "update-available"   => Ok(Self::UpdateAvailable),
             "update-required"    => Ok(Self::UpdateRequired),
-            "update-unavailable" => Ok(Self::UpdateUnavailable),
             "not-installed"      => Ok(Self::NotInstalled),
 
             _ => Err(LuaError::external(format!("invalid installation status: {s}")))
